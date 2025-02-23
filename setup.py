f6f804259b08bc85561f79cad9f42f8630fc42d0
Yasbantsamal22 <yasbantsamal62@gmail.com.com>
2025-02-23 11:55:30 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version='0.0.0',
+    author='Yasbant Samal',
+    author_email='yasbantsamal62@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
